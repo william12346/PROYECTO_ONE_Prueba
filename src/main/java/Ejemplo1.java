529195eba89665eadf0812cc7cc2b795026579a1
willy <willy@LAPTOP-6M6UQLK0>
2021-07-20 11:52:40 -0500

    Mi aporte muchachos

---
+++ src/main/java/Ejemplo1.java
@@ -0,0 +1,27 @@
+//LA ESTRUCTRUTURA BASE
+import java.util.Scanner;
+
+public class Ejemplo1 {
+   
+    public static void main(String[] args) {
+        System.out.println("hello world, como vamos mis panas");
+        System.out.print("la suma es: ");
+        System.out.print(1+2);
+    }
+    
+}
+
+
+//DECLARAR LOS DATOS
+/*
+public class Ejemplo1{
+    public static void main(String[] args){
+        int my_variables=60;
+        System.out.println(my_variables+6);
+    }
+}
+*/
+
+
+
+
